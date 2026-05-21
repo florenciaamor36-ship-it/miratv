cf94d1cc280a33ca2750807383a3790d358cd69e
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-05-20 23:41:06 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const TMDB_KEY = 'e1b746f9cd280db21c3616aff490340c';
-let favorites = JSON.parse(localStorage.getItem('iptv_fav') || '[]');
-let history = JSON.parse(localStorage.getItem('iptv_hist') || '[]');
